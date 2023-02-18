2822a7cf4d0bb129e86d2655f9540ae6134a1407
csubiii <csubicsabi05@gmail.com>
2023-02-18 12:46:59 +0100

    🚧 Navbar added

---
+++ arrp-duty-react/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-contenet'>
+        <ul className='navbar-nav' >
+          <li className='navbar-item'>
+            <p>Bejelenetkezés</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
