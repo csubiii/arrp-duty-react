bfd01b410e64d1426e39a9bfa14f7b3857ef252f
csubiii <csubicsabi05@gmail.com>
2023-04-02 17:47:47 +0200

    Timer Calc Fixed

--- arrp-duty-react/src/pages/duty/Duty.jsx
+++ arrp-duty-react/src/pages/duty/Duty.jsx
@@ -68,7 +68,7 @@ const Duty = () => {
             </div>
               <div className="duty-side">
                 <div className="duty-side-items">
-                <p className="duty-time-all">Szolgálati időd összesen: {Math.floor(list.dutyTime % (3600*24) / 3600)} óra {Math.floor(list.dutyTime % 3600 / 60)} perc {Math.floor(list.dutyTime % 60)} mp</p>
+                <p className="duty-time-all">Szolgálati időd összesen: {Math.floor(list.dutyTime / 3600)} óra {Math.floor(list.dutyTime % 3600 / 60)} perc {Math.floor(list.dutyTime % 3600 % 60)} mp</p>
                   <a href="https://www.google.com/forms/about/" target="_blank" className="report-btn" >Hibabejelentés</a>
                   {/*<Link className='leaderboard-btn' to="/ranglista" onClick={() => confirm("Akkor nézd meg a statisztikát ha jelenleg nem vagy szolgálatban!")}>Szolgálati Statisztika</Link>*/}
                   <button className="leaderboard-btn" onClick={handleConfirm}>Szolgálati Statisztika</button>
