e26fa2f8640297b4a4e889ef0877dbd530511ac2
csubiii <csubicsabi05@gmail.com>
2023-04-17 21:07:38 +0200

    ⛑ Maintenance Feature Added

--- arrp-duty-react/src/pages/Main.jsx
+++ arrp-duty-react/src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignUpLogIn from './SignUpLogIn'
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from "../config/firebase";
@@ -7,11 +7,15 @@ import Duty from './duty/Duty';
 
 const Main = () => {
 
+  const [ isHidden, setIsHidden ] = useState(false)
   const [user] = useAuthState(auth);
 
   return (
-    <>
+    <>    
+    { isHidden && <h1 className='maintenance-text'>Karbantartás alatt</h1> }
+    <div hidden={isHidden}>
     { user ? <Duty /> : <SignUpLogIn /> }
+    </div>
     </>
   )
 }
