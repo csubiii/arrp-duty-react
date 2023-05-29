445c4a526860a59d049ea72e5a87fcd00c0b9ef7
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2023-05-29 19:14:34 +0200

    4. ciklus

--- arrp-duty-react/src/pages/archive/ArchivedDutys.jsx
+++ arrp-duty-react/src/pages/archive/ArchivedDutys.jsx
@@ -30,220 +30,315 @@ const ArchivedDutys = () => {
     })
   );
 
-  const users = [
-    "lakatos zolika",
-    "Jonny Williems",
-    "Gery Miler",
-    "White Isaac",
-    "Daniel White",
-    "Rocky Rambo",
-    "James Watson",
-    "Burke Noel",
-    "Szurmaj Alfred",
-    "Noah Hill",
-    "Seranko Natasha",
-    "Carlos Ryan",
-    "James Willson",
-    "Thomas Orosz",
-    "Latorre Santiago",
-    "Lucifer Morningstar",
-    "Jake Hill",
-    "Andrew Carl",
-    "Mano Maki",
-    "James Bulger",
-    "csubi",
-    "Rantot Tamas",
-    "Lakatos Capy",
-    "William Brown",
-    "Alex Berger",
-    "Matthew Hardy",
-    "Szurmaj Romeo",
-    "Kiss Peter",
-    "Angster Jan",
-    "Itadori Yuuji",
-    "Reed John",
-    "Lorenzo Rossi",
-  ];
-
-  const dutyData = [
+  const dutyData3 = [
     {
       username: "Ryan Walken",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn - Távozás"
     },
     {
       username: "Lakatos Zolika",
-      time: "14 óra 1 perc 32 mp",
+      time: "14:01:32",
       comment: "-"
     },
     {
       username: "Jonny Williems",
-      time: "35 óra 21 perc 17 mp",
+      time: "35:21:17",
       comment: "Prémium IV."
     },
     {
       username: "Gery Miler",
-      time: "20 óra 23 perc 20 mp",
+      time: "20:23:20",
       comment: "-"
     },
     {
       username: "White Isaac",
-      time: "12 óra 58 perc 46 mp",
+      time: "12:58:46",
       comment: "-"
     },
     {
       username: "Daniel White",
-      time: "00 óra 00 perc 26 mp",
+      time: "00:00:26",
       comment: "Lefokozás"
     },
     {
       username: "Rocky Rambo",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn"
     },
     {
       username: "Burke Noel",
-      time: "46 óra 22 perc 24 mp",
+      time: "46:22:24",
       comment: "Prémium III."
     },
     {
       username: "Szurmaj Alfred",
-      time: "28 óra 37 perc 29 mp",
+      time: "28:37:29",
       comment: "Prémium V."
     },
     {
       username: "Adam Walker",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn - Távozás"
     },
     {
       username: "Noah Hill",
-      time: "19 óra 48 perc 29 mp",
+      time: "19:48:29",
       comment: "-"
     },
     {
       username: "Seranko Natasha",
-      time: "127 óra 52 perc 44 mp",
+      time: "127:52:44",
       comment: "Prémium I."
     },
     {
       username: "Carlos Ryan",
-      time: "15 óra 01 perc 57 mp",
+      time: "15:01:57",
       comment: "-"
     },
     {
       username: "James Willson",
-      time: "25 óra 45 perc 15 mp",
+      time: "25:45:15",
       comment: "-"
     },
     {
       username: "Latorre Santiago",
-      time: "00 óra 05 perc 01 mp",
+      time: "00:05:01",
       comment: "Szabadság"
     },
     {
       username: "Lucifer Morningstar",
-      time: "47 óra 39 perc 18 mp",
+      time: "47:39:18",
       comment: "Prémium II."
     },
     {
       username: "Jake Hill",
-      time: "04 óra 12 perc 44 mp",
+      time: "04:12:44",
       comment: "Ciklusbelépő"
     },
     {
       username: "Andrew Carl",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn"
     },
     {
       username: "Mano Maki",
-      time: "30 óra 07 perc 57 mp",
+      time: "30:07:57",
       comment: "-"
     },
     {
       username: "James Bulger",
-      time: "04 óra 34 perc 27 mp",
+      time: "04:34:27",
       comment: "Szabadság"
     },
     {
       username: "Rantot Tamas",
-      time: "28 óra 25 perc 22 mp",
+      time: "28:25:22",
       comment: "-"
     },
     {
       username: "Lakatos Capy",
-      time: "04 óra 05 perc 50 mp",
+      time: "04:05:50",
       comment: "Ciklusbelépő"
     },
     {
       username: "William Brown",
-      time: "00 óra 41 perc 22 mp",
+      time: "00:41:22",
       comment: "Ciklusbelépő"
     },
     {
       username: "Alex Berger",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn"
     },
     {
       username: "Matthew Hardy",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn"
     },
     {
       username: "Szurmaj Romeo",
-      time: "32 óra 18 perc 32 mp",
+      time: "32:18:32",
       comment: "-"
     },
     {
       username: "Angster Jan",
-      time: "15 óra 17 perc 03 mp",
+      time: "15:17:03",
       comment: "-"
     },
     {
       username: "Reed John",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Nyugállomány"
     },
     {
       username: "Lorenzo Rossi",
-      time: "23 óra 09 perc 00 mp",
+      time: "23:09:00",
       comment: "-"
     },
     {
       username: "James Watson",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Nem regisztrált"
     },
     {
       username: "Szop Tamás",
-      time: "00 óra 00 perc 00 mp",
+      time: "00:00:00",
       comment: "Warn - Távozás"
     },
    
   ];
 
-  const dutyData2 = [
+  const dutyData4 = [
     {
-      username: "csubi",
-      time: "100 óra 10 perc 10 mp",
-      comment: "100"
+      username: "Lakatos Zolika",
+      time: "00:31:19",
+      comment: "Szabadság"
     },
     {
-      username: "csubi",
-      time: "5 óra 5 perc 5 mp",
-      comment: "Hello"
+      username: "Jonny Williems",
+      time: "24:37:51",
+      comment: "Prémium III."
     },
     {
-      username: "csubi",
-      time: "9 óra 9 perc 9 mp",
-      comment: "9"
+      username: "Gery Miler",
+      time: "10:37:47",
+      comment: "-"
     },
     {
-      username: "csubi",
-      time: "100 óra 1 perc 1 mp",
-      comment: "test"
+      username: "White Isaac",
+      time: "10:51:52",
+      comment: "-"
+    },
+    {
+      username: "Daniel White",
+      time: "42:06:08",
+      comment: "Prémium II."
+    },
+    {
+      username: "Rocky Rambo",
+      time: "00:00:00",
+      comment: "Szabadság"
+    },
+    {
+      username: "James Watson",
+      time: "10:22:52",
+      comment: "-"
+    },
+    {
+      username: "Burke Noel",
+      time: "25:20:10",
+      comment: "Felmondás"
+    },
+    {
+      username: "Szurmaj Alfred",
+      time: "00:00:00",
+      comment: "Felmondás"
+    },
+    {
+      username: "Noah Hill",
+      time: "11:28:15",
+      comment: "-"
+    },
+    {
+      username: "Seranko Natasha",
+      time: "143:01:49",
+      comment: "Prémium I."
+    },
+    {
+      username: "Carlos Ryan",
+      time: "33:19:13",
+      comment: "Felmondás"
+    },
+    {
+      username: "James Willson",
+      time: "11:57:03",
+      comment: "-"
+    },
+    {
+      username: "Thomas Orosz",
+      time: "20:31:25",
+      comment: "Ciklusbelépő"
+    },
+    {
+      username: "Latorre Santiago",
+      time: "00:00:00",
+      comment: "2. Warn"
+    },
+    {
+      username: "Lucifer Morningstar",
+      time: "02:58:37",
+      comment: "Szabadság"
+    },
+    {
+      username: "Jake Hill",
+      time: "04:26:54",
+      comment: "1. Warn"
+    },
+    {
+      username: "Andrew Carl",
+      time: "00:00:00",
+      comment: "Felmondás"
+    },
+    {
+      username: "Mano Maki",
+      time: "00:00:00",
+      comment: "1. warn"
+    },
+    {
+      username: "James Bulger",
+      time: "09:19:09",
+      comment: "Szabadság"
+    },
+    {
+      username: "Rantot Tamas",
+      time: "12:30:31",
+      comment: "-"
+    },
+    {
+      username: "Lakatos Capy",
+      time: "06:55:29",
+      comment: "1. Warn"
+    },
+    {
+      username: "William Brown",
+      time: "00:00:00",
+      comment: "1. Warn"
+    },
+    {
+      username: "Alex Berger",
+      time: "00:00:00",
+      comment: "3. Warn - Távozás"
+    },
+    {
+      username: "Matthew Hardy",
+      time: "00:00:00",
+      comment: "2. Warn"
+    },
+    {
+      username: "Szurmaj Romeo",
+      time: "01:25:19",
+      comment: "Felmondás"
+    },
+    {
+      username: "Kiss Peter",
+      time: "15:56:41",
+      comment: "Ciklusbelépő"
+    },
+    {
+      username: "Angster Jan",
+      time: "00:00:00",
+      comment: "1. Warn"
+    },
+    {
+      username: "Itadori Yuuji",
+      time: "00:07:40",
+      comment: "Ciklusbelépő"
+    },
+    {
+      username: "Lorenzo Rossi",
+      time: "43:34:39",
+      comment: "Felmondás"
     },
    
   ];
@@ -262,7 +357,7 @@ const ArchivedDutys = () => {
         </thead>
         <tbody>
           {" "}
-          {dutyData.map((data, id) => {
+          {dutyData3.map((data, id) => {
             return (
               <tr key={id}>
                 <td> {data.username} </td>
@@ -285,7 +380,7 @@ const ArchivedDutys = () => {
         </thead>
         <tbody>
           {" "}
-          {dutyData2.map((data, id) => {
+          {dutyData4.map((data, id) => {
             return (
               <tr key={id}>
                 <td> {data.username} </td>
